e673b8b906603560cf76b318a25054bec3ed3814
datikken <tikken23@gmail.com>
2024-09-12 20:47:04 +0300

    3 modules https://go.dev/doc/tutorial/

---
+++ 0_modules/greetings/greetings_test.go
@@ -0,0 +1,41 @@
+package greetings
+
+import (
+	"regexp"
+	"slices"
+	"testing"
+)
+
+func TestRandomFormat(t *testing.T) {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	result := randomFormat()
+	if !slices.Contains(formats, result) {
+		t.Fatalf("randomFormat failed")
+	}
+}
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello("Gladys")
+	if !want.MatchString(msg) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v, want match for %#q, nil`, msg, err, want)
+	}
+}
+
+// TestHelloEmpty calls greetings.Hello with an empty string,
+// checking for an error.
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
+	}
+}
